793f1c5fe397df04476399cee1f5efd83ba3788f
ptandalanbanuasejahtera <pt.andalanbanuasejahtera@gmail.com>
2022-09-20 10:53:02 +0800

    adding components and content

---
+++ src/components/Info.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Info() {
+    return(
+        <div className="info">
+            <img src="../src/profile.jpg" alt="profile" />
+            <h1>Jannice Felicia</h1>
+            <h4>Frontend Developer</h4>
+            <p>jannicefelicia.website</p>
+            <a type="button" href="#">Email</a>
+            <a type="button" href="https://www.linkedin.com/in/jannice-felicia/">LinkedIn</a>
+        </div>
+    )
+}
